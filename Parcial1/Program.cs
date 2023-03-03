1217a652f77c93a50ea500717fc63d21c215f74f
Cristopher Leonardo Zepeda Esquivel (Leo) <crisleonardo100@gmail.com>
2023-03-03 15:57:03 -0600

    Add files via upload

---
+++ Parcial1/Program.cs
@@ -0,0 +1,80 @@
+﻿
+
+{
+    Console.WriteLine("Ingresa un numero entero positivo que sea mayor que 0 porfavor\n");
+    int n = int.Parse(Console.ReadLine());
+
+
+
+
+    
+
+
+
+    //Numeros Primos
+    Console.WriteLine("\n\nNumeros primos del 1 al " + n + ":\n");
+
+    for (int p = 2; p <= n; p++)
+    {
+        bool esPrimo = true;
+
+        for (int j = 2; j <= Math.Sqrt(p); j++)
+        {
+            if (p % j == 0)
+            {
+                esPrimo = false;
+                
+            }
+        }
+
+        if (esPrimo)
+        {
+            Console.Write("{0} ", p);
+        }
+    }
+
+    //Multiplos de 4
+    Console.WriteLine("\n\nNumeros que son multiplos de 4 del 1 al " + n+"\n");
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 4 == 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    Console.WriteLine("\n\nIncremento de 3 hasta llegar a " + n+"\n");
+    
+    int E = 1;
+    while (E <= n)
+    {
+        E += 3;
+        Console.WriteLine(E);
+    }
+
+
+    int a = 0;
+
+    Console.WriteLine("Su Numero es: " + n + "\n\nResultado: \n");
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        a++;
+        int C = i * i;
+        Console.WriteLine(a + "*" + a + " = " + C);
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
